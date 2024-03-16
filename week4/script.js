fa548dff4323d0ae66c47ddef6d04ebb732fade8
João Colonna <132366156+JoaoColonna@users.noreply.github.com>
2024-03-16 11:04:13 -0300

    fix: telefone mask ex1 week2

--- week4/script.js
+++ week4/script.js
@@ -13,6 +13,19 @@ function setTipoUsuario(param){
     }
 }
 
+const handlePhone = (event) => {
+  let input = event.target
+  input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
+
 function gerarRegistro(){
     let tipoPessoa = document.getElementById('aluno');
     if(tipoPessoa.checked == true){
